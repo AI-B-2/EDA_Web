99115d1e0afd46691dcc4fd775ee7f36fbcf7c38
dltnwls9623 <dltnwls9623@gmail.com>
2021-05-21 19:44:31 +0900

    add files

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import plotly, chart_studio
 import chart_studio.plotly as py
 import plotly.graph_objs as go
 import numpy as np
-
+from confirmed_plot_map import plot_map, tree_map, confirmed_over_time
 app = Flask(__name__)
 
 # GET /
 @app.route('/')
-def homepage():
+def eda():
+    '''
     count = 500
     xScale = np.linspace(0, 100, count)
     yScale = np.arange(1, 100)
@@ -22,7 +23,12 @@ def homepage():
  
     data = [trace]
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('index.html', plot=graphJSON)
+    return render_template('eda.html', plot=graphJSON)
+    '''
+    map_graphJSON = plot_map()
+    tree_graphJSON = tree_map()
+    time_graphJSON = confirmed_over_time()
+    return render_template('eda.html', plot={'mapgraph': map_graphJSON, 'treegraph':tree_graphJSON, 'timegraph':time_graphJSON})
 
 
 if __name__ == '__main__':
