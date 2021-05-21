87a1d5633781c2edd5888702b3533ff13f0b3d09
dltnwls9623 <dltnwls9623@gmail.com>
2021-05-22 01:33:50 +0900

    fix some errors

--- confirmed_plot_map.py
+++ confirmed_plot_map.py
@@ -1,23 +1,11 @@
 import pandas as pd
 import numpy as np
-# to load json files
 import json
-# datetime oprations
-from datetime import timedelta
-import datetime as dt
-import plotly, chart_studio
+import plotly
 import chart_studio.plotly as py
 import plotly.express as px
-import plotly.graph_objs as go
-# import plotly.figure_factory as ff
-from plotly.subplots import make_subplots
-# for offline ploting
-from plotly.offline import plot, iplot, init_notebook_mode
-init_notebook_mode(connected=True)
 import json
 
-
-full_grouped = pd.read_csv('full_grouped.csv')
 
 def plot_map():
     df=pd.read_csv('country_wise_latest.csv')
@@ -26,8 +14,7 @@ def plot_map():
     
     df = df[df[col]>0]
     fig = px.choropleth(df, locations="Country/Region", locationmode='country names', 
-                  color=col, hover_name="Country/Region", 
-                  title=col, hover_data=[col], color_continuous_scale=pal)
+                  color=col, hover_name="Country/Region", hover_data=[col], color_continuous_scale=pal)
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
@@ -51,7 +38,7 @@ def confirmed_over_time():
     df["Date"].dt.strftime('%Y-%m-%D')
     frame = df["Date"].dt.strftime('%Y-%m-%D')
     
-    fig = px.choropleth(df, locations="Country/Region", color=np.log(df["Confirmed"]), locationmode='country names', hover_name="Country/Region", animation_frame=frame, title='Cases over time', color_continuous_scale=px.colors.sequential.matter)
+    fig = px.choropleth(df, locations="Country/Region", color=np.log(df["Confirmed"]), locationmode='country names', hover_name="Country/Region", animation_frame=frame, color_continuous_scale=px.colors.sequential.matter)
     fig.update(layout_coloraxis_showscale=False)
     
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
