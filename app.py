ac8fd051afbabe3c7678a19ec5744f2c428f414f
yuseon27 <ysnam19@naver.com>
2021-05-21 19:53:55 +0900

    merged g Please enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import chart_studio.plotly as py
 import plotly.graph_objs as go
 import numpy as np
 from confirmed_plot_map import plot_map, tree_map, confirmed_over_time
+
 app = Flask(__name__)
 
 # GET /
 @app.route('/')
 def eda():
-
-    map_graphJSON = plot_map()
+    map_graphJSON  = plot_map()
     tree_graphJSON = tree_map()
     time_graphJSON = confirmed_over_time()
     return render_template('eda.html', plot={'mapgraph': map_graphJSON, 'treegraph':tree_graphJSON, 'timegraph':time_graphJSON})    
